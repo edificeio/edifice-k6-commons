9fca4fe03c55889d9820b80a3b075ab940cbc284
mariusestaque <marius.estaque@opendigitaleducation.com>
2025-01-28 14:35:41 +0100

    fix: WB-3639, update javadoc

--- src/shares.utils.ts
+++ src/shares.utils.ts
@@ -5,32 +5,32 @@ import { Shares } from "./models";
 const rootUrl = __ENV.ROOT_URL;
 
 /**
- * Method applying share rights to a file in workspace
- * @param fileId the id of the file to apply share rights to
+ * Method applying share rights to a document in workspace
+ * @param id the id of the document to apply share rights to
  * @param shares the share rights
  * @returns the http response to the endpoint request
  */
 export function shareFile(
-  fileId: string,
+  id: string,
   shares: Shares,
 ): RefinedResponse<any> {
   const headers = getHeaders();
   headers["content-type"] = "application/json";
   const payload = JSON.stringify(shares);
-  return http.put(`${rootUrl}/workspace/share/resource/${fileId}`, payload, {
+  return http.put(`${rootUrl}/workspace/share/resource/${id}`, payload, {
     headers,
   });
 }
 
 /**
- * Method returning the share rights of a file stored in workspace
- * @param fileId the id of the file whose share rights must be fetched
- * @returns the share rights of a file in workspace
+ * Method returning the share rights of a document stored in workspace
+ * @param id the id of the file whose share rights must be fetched
+ * @returns the share rights of a document in workspace
  */
-export function getShares(fileId: string): Shares {
+export function getShares(id: string): Shares {
   const headers = getHeaders();
-  // the empty 'search' query param is mandatory to retrieve the actual share rights of the file.
-  let res = http.get(`${rootUrl}/workspace/share/json/${fileId}?search=`, {
+  // the empty 'search' query param is mandatory to retrieve the actual share rights of the document.
+  let res = http.get(`${rootUrl}/workspace/share/json/${id}?search=`, {
     headers,
   });
   return JSON.parse(<string>res.body);
