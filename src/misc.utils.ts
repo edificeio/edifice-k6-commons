8581a20ee642dc81f73032f9743e47c51029c527
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2025-05-16 18:05:43 +0200

    feat: factorize constants and improve oauth2 authentication

---
+++ src/misc.utils.ts
@@ -0,0 +1,30 @@
+import http, { RefinedResponse } from "k6/http";
+import { getHeaders } from "./user.utils";
+import { CLIENT_SCOPE, CLIENT_SECRET } from "./env.utils";
+const rootUrl = __ENV.ROOT_URL;
+
+export type OauthClientRegistrationRequest = {
+    name: string,
+    grantType? :string,
+    code? :string,
+    secret?: string,
+    address?:string,
+    scope?: string
+}
+
+export function createOAuthClient(request: OauthClientRegistrationRequest, structureId?: string): RefinedResponse<undefined> {
+  const res = http.post(
+    `${rootUrl}/appregistry/application/external${structureId ? '?structureId=' + structureId : ''}`,
+    JSON.stringify({
+        ...request,
+        grantType: request.grantType || "password",
+        secret: request.secret || CLIENT_SECRET,
+        address: request.address || "http://localhost:8090",
+        scope: request.scope || CLIENT_SCOPE,
+    }),
+    {
+      headers: {...getHeaders(), "Content-Type": "application/json"},
+    },
+  );
+  return res;
+}
